2dfa6246e34ff15a1546cab0ca4247fd258d7412
Evan Cooney <evancooney@gmail.com>
2025-02-16 23:15:14 -0500

    hapi

---
+++ hapi/src/index.ts
@@ -0,0 +1,25 @@
+import Hapi from '@hapi/hapi';
+
+const start = async function(): Promise<void> {
+    const server: Hapi.Server = Hapi.server({
+        port: 4000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Hello, world!';
+        }
+    });
+
+    await server.start();
+
+    console.log(`Server started on: ${server.info.uri}`);
+};
+
+start().catch(err => {
+    console.error(err);
+    process.exit(1);
+});
