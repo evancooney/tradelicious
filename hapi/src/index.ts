984c857322cfa34ce5c1eaceefda3d65bfa6dec6
Evan Cooney <evancooney@gmail.com>
2025-02-17 00:16:30 -0500

    sss

--- hapi/src/index.ts
+++ hapi/src/index.ts
@@ -3,7 +3,7 @@ import Hapi from '@hapi/hapi';
 const start = async function(): Promise<void> {
     const server: Hapi.Server = Hapi.server({
         port: 4000,
-        host: '0,0.0.0'
+        host: '0.0.0.0'
     });
 
     server.route({
