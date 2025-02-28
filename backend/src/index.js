dd750cf8f12101cb64909afa378781f1cfa3a423
Evan Cooney <evancooney@gmail.com>
2025-02-27 21:53:00 -0500

    adding the triple zeros

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import app from './app.js';
 
 // Start the server
 const port = 3333;
-app.listen(port,  () => {
-    console.log(`Server is running on http://localhost:${port}`);
+const host='0.0.0.0';
+app.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
 });
